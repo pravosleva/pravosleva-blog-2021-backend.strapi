4515dfae972df4ba192a01c1a60a25d41fbd5fc3
Den <selection4test@gmail.com>
2023-11-19 01:26:10 +0300

    start details updated

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: 'https://pravosleva.pro/strapi',
+  url: env('SERVER_PUBLIC_URL', ''),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
